cdee036e60217e3e55d6261a8c152576a41fba26
Nikita Tseplyaev <nikitatsp@mail.ru>
2024-06-26 23:18:09 +0300

    Initial Commit

---
+++ DebtAccounting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DebtAccounting
+//
+//  Created by Никита Цепляев on 26.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
